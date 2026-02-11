8488020874a4baf21154e1df398be3e15d6f2047
slin1863 <sxuelin.1863@gmail.com>
2026-02-10 21:53:57 -0600

    connect database

--- db.py
+++ db.py
@@ -1,1 +1,18 @@
-print("Hello world")
+import psycopg2
+from psycopg2 import sql
+
+try:
+    conn = psycopg2.connect(
+        dbname = "photon",
+        user = "student",
+        password = "student",
+        host = "localhost",
+    )
+
+except Exception as error:
+    print(f"Error connecting to PostgreSQL daatabase: {error}")
+
+finally:
+    if conn:
+        conn.close()
+        print("Connection closed.")
