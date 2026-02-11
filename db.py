3039401e8c273457197c37b2b7fe27dabd0b24ca
slin1863 <sxuelin.1863@gmail.com>
2026-02-10 22:40:33 -0600

    test link database

--- db.py
+++ db.py
@@ -1,18 +1,29 @@
+# Must install psycopg2 package on machine
+# sudo apt-get install python3-psycopg2
 import psycopg2
 from psycopg2 import sql
 
-try:
-    conn = psycopg2.connect(
-        dbname = "photon",
-        user = "student",
-        password = "student",
-        host = "localhost",
-    )
+if __name__ == '__main__':
+    # Connect to database
+    try:
+        conn = psycopg2.connect(
+            dbname = "photon",
+            user = "student",
+            password = "student",
+            host = "localhost",
+        )
+        cursor = conn.cursor()
 
-except Exception as error:
-    print(f"Error connecting to PostgreSQL daatabase: {error}")
+        print("Successfully connected to database.")
+        
+        cursor.execute("SELECT version();")
+        version = cursor.fetchone()
+        print(f"Connected to - {version}")
 
-finally:
-    if conn:
-        conn.close()
-        print("Connection closed.")
+    except Exception as error:
+        print(f"Error connecting to PostgreSQL daatabase: {error}")
+
+    finally:
+        if conn:
+            conn.close()
+            print("Connection closed.")
