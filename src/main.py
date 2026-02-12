72eda0de7c9166b32c044d92b4a10cdc0c9d2a1b
Daniel Nkunga <daniel.s.nkunga@gmail.com>
2026-02-12 15:43:28 -0600

    Ugly table with 'start game' button
    No CSS. Button kills program; header cells need to be locked; player entry textbox yet to be implemented

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from PySide6 import QtWidgets, QtCore, QtGui
-from PySide6.QtWidgets import *
+from PySide6.QtWidgets import * # TODO: make verbose
 
 import sys
 
@@ -11,22 +11,25 @@ class MainWindow(QtWidgets.QWidget):
         self.setWindowTitle("Player Entry Screen")
 
         self.resize(720, 643)
-        # self.button = QtWidgets.QPushButton("Click me!")
-        # self.text = QtWidgets.QLabel("Hello World",
-        #                              alignment=QtCore.Qt.AlignmentFlag.AlignCenter)
-
-        self.createTable()
 
         self.layout = QtWidgets.QVBoxLayout(self)
-        self.layout.addWidget(self.tableWidget)
-        # layout.addWidget(self.text)
-        # layout.addWidget(self.button)
+        self.text = QtWidgets.QLabel("Edit Current Game", alignment=QtCore.Qt.AlignmentFlag.AlignHCenter)
+        self.layout.addWidget(self.text)
+
+        self.create_table()
+
+        # Start game button
+        self.button = QtWidgets.QPushButton("Start Game")
+        self.button.clicked.connect(self.start_game)
+        self.layout.addWidget(self.button)
+
 
         self.setLayout(self.layout)
 
     def showEvent(self, event: QtGui.QShowEvent):
         super().showEvent(event)
 
+        # layout.addWidget(self.button)
         # show splash screen when the main window is shown
         label = QtWidgets.QLabel(self)
         pixmap = QtGui.QPixmap('res/splash_screen.jpg').scaled(725, 648)
@@ -35,35 +38,39 @@ class MainWindow(QtWidgets.QWidget):
         label.show()
 
         # close splash screen after 3 seconds
-        QtCore.QTimer.singleShot(3000, label.close)
+        QtCore.QTimer.singleShot(300, label.close)
 
-        self.text = QtWidgets.QLabel("Edit Current Game", alignment=QtCore.Qt.AlignmentFlag.AlignHCenter)
-        self.layout.addWidget(self.text)
         pass
 
     #Create table
-    def createTable(self):
+    def create_table(self):
         self.tableWidget = QTableWidget()
 
         #Row count
-        self.tableWidget.setRowCount(4)
+        self.tableWidget.setRowCount(15)
 
         #Column count
-        self.tableWidget.setColumnCount(2)
+        self.tableWidget.setColumnCount(6)
 
-        self.tableWidget.setItem(0,0, QTableWidgetItem("Name"))
-        self.tableWidget.setItem(0,1, QTableWidgetItem("City"))
-        self.tableWidget.setItem(1,0, QTableWidgetItem("Aloysius"))
-        self.tableWidget.setItem(1,1, QTableWidgetItem("Indore"))
-        self.tableWidget.setItem(2,0, QTableWidgetItem("Alan"))
-        self.tableWidget.setItem(2,1, QTableWidgetItem("Bhopal"))
-        self.tableWidget.setItem(3,0, QTableWidgetItem("Arnavi"))
-        self.tableWidget.setItem(3,1, QTableWidgetItem("Mandsaur"))
- 
+        self.tableWidget.setItem(0,0, QTableWidgetItem("RED TEAM")) # TODO: lock cells
+        self.tableWidget.setItem(0,3, QTableWidgetItem("GREEN TEAM"))
+        self.tableWidget.setItem(1,0, QTableWidgetItem("PLAYER ID"))
+        self.tableWidget.setItem(1,1, QTableWidgetItem("CODENAME"))
+        self.tableWidget.setItem(1,2, QTableWidgetItem("EQUIPMENT ID"))
+        self.tableWidget.setItem(1,3, QTableWidgetItem("PLAYER ID"))
+        self.tableWidget.setItem(1,4, QTableWidgetItem("CODENAME"))
+        self.tableWidget.setItem(1,5, QTableWidgetItem("CODENAME"))
+
         #Table will fit the screen horizontally
         self.tableWidget.horizontalHeader().setStretchLastSection(True)
         self.tableWidget.horizontalHeader().setSectionResizeMode(
             QHeaderView.Stretch)
+
+        self.layout.addWidget(self.tableWidget)
+
+    def start_game(self):
+        # TODO: actually start the game, currently just ends the program
+        self.close()
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
