a2354ff795dfd31c2bc70170bf959fd620edf733
amadeusdotpng <amadeus8182@gmail.com>
2026-04-09 13:55:15 -0500

    rename `ui/entry_terminal.py` to `ui/entry.py`
    I was acquainted with all Petersburg as it was; that was why I felt as
    though they were all deserting me when all Petersburg packed up and went
    to its summer villa.

--- src/ui/__init__.py
+++ src/ui/__init__.py
@@ -1,3 +1,3 @@
-from ui.entry_terminal import EntryWindow
+from ui.entry import EntryWindow
 from ui.game import GameWindow
 from ui.dialogs import AddPlayerDialog, AddCodenameDialog, ChangeUDPNetworkDialog
